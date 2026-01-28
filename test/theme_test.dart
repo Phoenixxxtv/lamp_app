78dd69cfc0d7deccc357c6a4999ac43efdfd32dd
Lakshman Reddy <lakshmanreddy@Lakshmans-MacBook-Air.local>
2026-01-28 18:36:07 +0530

    fix: Initialize Flutter bindings in theme_test.dart

--- test/theme_test.dart
+++ test/theme_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lamp_app/core/theme/app_theme.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('AppTheme', () {
     test('Primary color is correct', () {
       expect(AppColors.primary, const Color(0xFF6B4EAB));
